c378d29b6ae56fd0d67aa366823757d18e6dfd0f
권용현 <68382593+yhkwon2004@users.noreply.github.com>
2024-09-24 14:56:06 +0900

    실습파일 학생 3명의 리스트 입,출력

---
+++ 4주차_실습_학생리스트.py
@@ -0,0 +1,12 @@
+student = []
+
+for i in range(3):
+    name = input(f"{i+1}번 학생의 이름: ")
+    stu_num = input(f"{i+1}번 학생의 학번: ")
+    phone = input(f"{i+1}번 학생의 전화번호: ")
+    
+    student.append({"이름": name, "학번": stu_num, "전화번호": phone})
+
+for i in range(3):
+    print("\n",f"{i+1}번 학생 정보")
+    print(student[i])
